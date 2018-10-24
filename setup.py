df97cfd58fa97e3a4e4463f1d27a8280d04b1cba
Bo Bayles <bbayles@gmail.com>
2018-10-23 21:07:50 -0500

    Let's just classify for Python 3

--- setup.py
+++ setup.py
@@ -39,9 +39,6 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
     ],
     entry_points={
         'console_scripts': [
