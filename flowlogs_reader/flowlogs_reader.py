68b2d2b59252138d8e65481de51a3083de2d38d8
bthacker <brathack@cisco.com>
2022-12-13 13:59:16 -0500

    removed unused

--- flowlogs_reader/flowlogs_reader.py
+++ flowlogs_reader/flowlogs_reader.py
@@ -354,7 +354,7 @@ class FlowLogsReader(BaseReader):
                         try: 
                             flow = FlowRecord.from_cwl_event(event, self.fields)
                             yield flow
-                        except ValueError as ve:
+                        except ValueError:
                             print('[ERROR] FlowRecord line ValueError, continuing...')
                             continue
                         
@@ -363,7 +363,7 @@ class FlowLogsReader(BaseReader):
                 try: 
                     flow = FlowRecord.from_cwl_event(event, self.fields)
                     yield flow
-                except ValueError as ve:
+                except ValueError:
                     print('[ERROR] FlowRecord line ValueError, continuing...')
                     continue
 
@@ -504,6 +504,6 @@ class S3FlowLogsReader(BaseReader):
             try: 
                 flow = FlowRecord(event_data)
                 yield flow
-            except ValueError as ve:
+            except ValueError:
                 print('[ERROR] FlowRecord line ValueError, continuing...')
                 continue
